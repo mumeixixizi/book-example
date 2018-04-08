350a7ac39be4ddf5e1e40ed4990f82037556d47e
Y.F.Liu <13816467925@139.com>
2018-04-08 14:42:43 +0800

    modify sth.

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -26,11 +27,16 @@ class NewVisitorTest(unittest.TestCase):
 
         inputbox.send_keys(Keys.ENTER)
 
+        time.sleep(5)
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows),
-                        'New to-do item did not appear in table')
+        # self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows),
+        #                 "New to-do item did not appear in table -- its text was:\n%s" % (
+        #                     table.text,
+        #                 ))
 
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
         self.fail('Finish the test!')
 
 

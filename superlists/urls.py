39c1691c91ac0b501a832bd0c2fd54a7eafc8a88
Y.F.Liu <13816467925@139.com>
2018-04-16 18:22:59 +0800

    modify sth.

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+import lists.urls
 
 urlpatterns = patterns('',
     # Examples:
@@ -8,7 +9,5 @@ urlpatterns = patterns('',
 
     # url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'lists.views.home_page', name='home'),
-    url(r'^lists/(\d+)/$', 'lists.views.view_list', name='view_list'),
-    url(r'^lists/(\d+)/add_item$', 'lists.views.add_item', name='add_item'),
-    url(r'^lists/new$', 'lists.views.new_list', name='new_list'),
+    url(r'^lists/', include(lists.urls)),
 )
